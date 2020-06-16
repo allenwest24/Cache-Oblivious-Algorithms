2279f4004eedba41cd91ad6ae2ecaacc8b1e57fe
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-06-16 13:13:12 -0400

    Add files via upload

--- Matrix-Transpose/main.cpp
+++ Matrix-Transpose/main.cpp
@@ -2,85 +2,81 @@
 #include <stdio.h> 
 #include <stdlib.h>
 
-#define M 4
 #define N 4
   
-void transpose(double* A, double* B) 
-{ 
+void transpose(double* A, double* B) { 
     int ii, jj; 
-    for (ii = 0; ii < N; ii++) 
-        for (jj = 0; jj < M; jj++) 
-            B[(ii * N) + jj] = A[(jj * M) + ii]; 
+    for (ii = 0; ii < N; ii++) { 
+        for (jj = 0; jj < N; jj++) { 
+            B[(ii * N) + jj] = A[(jj * N) + ii];
+	} 
+    }
 }
 
-void CacheObliviousTransposition(int x, int delx, int y, int dely, double* matrix) {
-    if ((delx == 1) && (dely == 1)) {
-        if(x<y)
-        {
-            double tmp = matrix[(N*y) + x];
-            matrix[(N*y) + x] = matrix[(N*x) + y];
-            matrix[(N*x) + y] = tmp;
+void CacheObliviousTransposition(int x, int hix, int y, int hiy, double* matrix) {
+    if ((hix == 1) && (hiy == 1)) {
+	if(x < y) {
+            double tmp = matrix[(N * y) + x];
+            matrix[(N * y) + x] = matrix[(N * x) + y];
+            matrix[(N * x) + y] = tmp;
         }
         return;
     }
 
-    if (delx >= dely) {
-        int xmid = delx / 2;
-        CacheObliviousTransposition(x, xmid, y, dely, matrix);
-        CacheObliviousTransposition(x + xmid, delx - xmid, y, dely, matrix);
+    if (hix >= hiy) {
+        int xmid = hix / 2;
+        CacheObliviousTransposition(x, xmid, y, hiy, matrix);
+        CacheObliviousTransposition(x + xmid, hix - xmid, y, hiy, matrix);
         return;
     }
 
-    int ymid = dely / 2;
-    CacheObliviousTransposition(x, delx, y, ymid, matrix);
-    CacheObliviousTransposition(x, delx, y + ymid, dely - ymid, matrix);
+    int ymid = hiy / 2;
+    CacheObliviousTransposition(x, hix, y, ymid, matrix);
+    CacheObliviousTransposition(x, hix, y + ymid, hiy - ymid, matrix);
 }
   
-int main() 
-{ 
-
-    double A[N][M];
-    double B[N][M];
-    double C[N][M];
-
-    for(int i =0; i < N; ++i){
-        for(int j =0; j < M; ++j){
-            A[i][j] = (double)rand() / (double)RAND_MAX;
-            B[i][j] = (double)rand() / (double)RAND_MAX;
-	    C[i][j] = (double)rand() / (double)RAND_MAX;
+int main() { 
+    double A[N][N];
+    double B[N][N];
+    double C[N][N];
+    for(int ii = 0; ii < N; ++ii){
+        for(int jj = 0; jj < N; ++jj){
+            A[ii][jj] = (double)rand() / (double)RAND_MAX;
+            B[ii][jj] = (double)rand() / (double)RAND_MAX;
+	    C[ii][jj] = (double)rand() / (double)RAND_MAX;
         }
     }
-    int i, j; 
+    int kk, ll; 
 
     printf("A matrix was \n");
-    for (i = 0; i < N; i++)
-    {
-        for (j = 0; j < M; j++)
-        printf("%f ", A[i][j]);
-        printf("\n");
+    for (kk = 0; kk < N; kk++) {
+        for (ll = 0; ll < N; ll++) {
+            printf("%f ", A[kk][ll]);
+	}
+	printf("\n");
     }
     transpose((double *)A, (double *)B);
     printf("B matrix is \n"); 
-    for (i = 0; i < N; i++) 
-    { 
-        for (j = 0; j < M; j++) 
-        printf("%f ", B[i][j]); 
-        printf("\n"); 
+    for (kk = 0; kk < N; kk++) { 
+        for (ll = 0; ll < N; ll++) {
+            printf("%f ", B[kk][ll]);  
+	}
+	printf("\n");
     } 
     printf("C matrix was \n");
-    for (i = 0; i < N; i++)
-    {
-        for (j = 0; j < M; j++)
-        printf("%f ", C[i][j]);
-        printf("\n");
+    for (kk = 0; kk < N; kk++) {
+        for (ll = 0; ll < N; ll ++) {
+            printf("%f ", C[kk][ll]);
+	}
+	printf("\n");
     }
     CacheObliviousTransposition(0, N, 0, N,(double*) C);
     printf("C matrix is \n");
-    for (i = 0; i < N; i++)
-    {
-        for (j = 0; j < M; j++)
-        printf("%f ", C[i][j]);
-        printf("\n");
+    for (kk = 0; kk < N; kk++) {
+        for (ll = 0; ll < N; ll++) {
+            printf("%f ", C[kk][ll]);
+	}
+	printf("\n");
     }
   
     return 0; 
