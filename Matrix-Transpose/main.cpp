6671b880bfa03ba2fd8c859c91bc56d60db22102
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-06-16 13:52:24 -0400

    Update main.cpp

--- Matrix-Transpose/main.cpp
+++ Matrix-Transpose/main.cpp
@@ -1,4 +1,3 @@
-
 #include <stdio.h> 
 #include <stdlib.h>
 
@@ -13,7 +12,28 @@ void transpose(double* A, double* B) {
     }
 }
 
-void CacheObliviousTransposition(int x, int hix, int y, int hiy, double* matrix) {
+void cacheObliviousTranspose(int x, int hix, int y, int hiy, double* matrix, double* out) {
+    if ((hix == 1) && (hiy == 1)) {
+	if(x < y) {
+            out[(N * x) + y] = matrix[(N * y) + x];
+            out[(N * y) + x] = matrix[(N * x) + y];
+        }
+        return;
+    }
+
+    if (hix >= hiy) {
+        int xmid = hix / 2;
+        cacheObliviousTranspose(x, xmid, y, hiy, matrix, out);
+        cacheObliviousTranspose(x + xmid, hix - xmid, y, hiy, matrix, out);
+        return;
+    }
+
+    int ymid = hiy / 2;
+    cacheObliviousTranspose(x, hix, y, ymid, matrix, out);
+    cacheObliviousTranspose(x, hix, y + ymid, hiy - ymid, matrix, out);
+}
+
+void inPlaceCacheObliviousTranspose(int x, int hix, int y, int hiy, double* matrix) {
     if ((hix == 1) && (hiy == 1)) {
 	if(x < y) {
             double tmp = matrix[(N * y) + x];
@@ -25,30 +45,34 @@ void CacheObliviousTransposition(int x, int hix, int y, int hiy, double* matrix)
 
     if (hix >= hiy) {
         int xmid = hix / 2;
-        CacheObliviousTransposition(x, xmid, y, hiy, matrix);
-        CacheObliviousTransposition(x + xmid, hix - xmid, y, hiy, matrix);
+        inPlaceCacheObliviousTranspose(x, xmid, y, hiy, matrix);
+        inPlaceCacheObliviousTranspose(x + xmid, hix - xmid, y, hiy, matrix);
         return;
     }
 
     int ymid = hiy / 2;
-    CacheObliviousTransposition(x, hix, y, ymid, matrix);
-    CacheObliviousTransposition(x, hix, y + ymid, hiy - ymid, matrix);
+    inPlaceCacheObliviousTranspose(x, hix, y, ymid, matrix);
+    inPlaceCacheObliviousTranspose(x, hix, y + ymid, hiy - ymid, matrix);
 }
   
 int main() { 
     double A[N][N];
     double B[N][N];
     double C[N][N];
+    double D[N][N];
+    double E[N][N];
     for(int ii = 0; ii < N; ++ii){
         for(int jj = 0; jj < N; ++jj){
             A[ii][jj] = (double)rand() / (double)RAND_MAX;
             B[ii][jj] = (double)rand() / (double)RAND_MAX;
 	    C[ii][jj] = (double)rand() / (double)RAND_MAX;
+            D[ii][jj] = (double)rand() / (double)RAND_MAX;
+            E[ii][jj] = (double)rand() / (double)RAND_MAX;
         }
     }
     int kk, ll; 
 
-    printf("A matrix was \n");
+    printf("Before transpose() \n");
     for (kk = 0; kk < N; kk++) {
         for (ll = 0; ll < N; ll++) {
             printf("%f ", A[kk][ll]);
@@ -56,29 +80,46 @@ int main() {
 	printf("\n");
     }
     transpose((double *)A, (double *)B);
-    printf("B matrix is \n"); 
+    printf("After transpose() \n"); 
     for (kk = 0; kk < N; kk++) { 
         for (ll = 0; ll < N; ll++) {
             printf("%f ", B[kk][ll]);  
 	}
 	printf("\n");
-    } 
-    printf("C matrix was \n");
+    }
+	
+    printf("Before cacheObliviousTranspose() \n");
     for (kk = 0; kk < N; kk++) {
         for (ll = 0; ll < N; ll ++) {
             printf("%f ", C[kk][ll]);
 	}
 	printf("\n");
     }
-    CacheObliviousTransposition(0, N, 0, N,(double*) C);
-    printf("C matrix is \n");
+    cacheObliviousTranspose(0, N, 0, N,(double*) C, (double*) D);
+    printf("After cacheObliviousTranspose()\n");
     for (kk = 0; kk < N; kk++) {
         for (ll = 0; ll < N; ll++) {
-            printf("%f ", C[kk][ll]);
+            printf("%f ", D[kk][ll]);
+	}
+	printf("\n");
+    }
+	
+	
+    printf("Before inPlaceCacheObliviousTranspose() \n");
+    for (kk = 0; kk < N; kk++) {
+        for (ll = 0; ll < N; ll ++) {
+            printf("%f ", E[kk][ll]);
+	}
+	printf("\n");
+    }
+    inPlaceCacheObliviousTranspose(0, N, 0, N,(double*) E);
+    printf("After inPlaceCacheObliviousTranspose()\n");
+    for (kk = 0; kk < N; kk++) {
+        for (ll = 0; ll < N; ll++) {
+            printf("%f ", E[kk][ll]);
 	}
 	printf("\n");
     }
   
     return 0; 
 } 
-
